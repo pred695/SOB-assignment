15c71fd1283908510a4e3cf16d21150d596a6549
Zaid <predcodes695@gmail.com>
2024-04-30 21:14:13 +0530

    final push, lets hope for the best

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
-import "github.com/pred695/code-challenge-2024-pred695/Blockchain"
+import (
+	"github.com/pred695/code-challenge-2024-pred695/Blockchain"
+)
 
 func main() {
 	Blockchain.MineBlock()
